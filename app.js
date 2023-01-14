4c4e39161cd2fc41c47d0d3d486e0eba9d6a33fb
steafanpt0 <steafansteinocher@gmail.com>
2023-01-14 16:34:50 -0600

    added styles html and declared variables

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Working');
+let currentNumber = '';
+let previousNumber = '';
+let opperator = '';
+
+const displayTop = document.querySelector('.top-number');
+const displayBottom = document.querySelector('.bottom-number');
+
+const numberBtn = document.querySelectorAll('.number');
+
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+const powerBtn = document.querySelector('.power');
+const divideBtn = document.querySelector('.divide');
+const addBtn = document.querySelector('.add');
+const subtractBtn = document.querySelector('.subtract');
+const multiplyBtn = document.querySelector('.multiply');
+const decimalBtn = document.querySelector('.decimal');
+const equalBtn = document.querySelector('.equals');
+
